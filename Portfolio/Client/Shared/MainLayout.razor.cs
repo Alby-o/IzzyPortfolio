5712317c45de083240172bc9d89c40905e670db4
Alexander Blyth <alex@3cssoftware.com.au>
2022-07-03 12:15:14 +1000

    Update theme for mobile styles

---
+++ Portfolio/Client/Shared/MainLayout.razor.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+using Portfolio.Client.Code;
+using Portfolio.Shared;
+using Portfolio.Shared.Entities;
+
+namespace Portfolio.Client.Shared
+{
+    public partial class MainLayout : IDisposable
+    {
+        private static List<MenuItem> MenuItems => MenuProvider.MenuItems;
+        private static SitePortfolio Portfolio => PortfolioProvider.Portfolio;
+
+        private Theme? selectedTheme = null;
+        private Theme? menuHoverTheme = null;
+
+        private Theme? Theme => selectedTheme ?? menuHoverTheme;
+
+        private MenuItem? selected = null;
+
+        private bool menuOpen;
+
+        [Inject] public NavigationManager NavManager { get; set; } = default!;
+
+        protected override void OnInitialized()
+        {
+            NavManager.LocationChanged += NavManager_LocationChanged;
+            SetMenuOpen();
+        }
+
+        private void NavManager_LocationChanged(object? sender, LocationChangedEventArgs e)
+        {
+            // Update the selected nav item & rerender
+            SetMenuOpen();
+            StateHasChanged();
+        }
+
+
+        private void SetMenuOpen()
+        {
+            var uri = new Uri(NavManager.Uri);
+            var selectedItem = MenuItems.FirstOrDefault(i => uri.LocalPath.Contains(i.Url));
+
+            menuOpen = selectedItem == null;
+
+            // If the menu has been closed, update the selected menu item. 
+            if (!menuOpen)
+            {
+                selectedTheme = selectedItem?.Theme;
+                selected = selectedItem;
+            }
+            else
+            {
+                // When the menu is opened, selected is not set to null as it will play its dissapear animation
+                selectedTheme = null;
+            }
+        }
+
+        public void Dispose()
+        {
+            NavManager.LocationChanged -= NavManager_LocationChanged;
+        }
+    }
+}
--- Portfolio/Client/Shared/Menu.razor.cs
+++ Portfolio/Client/Shared/Menu.razor.cs
@@ -4,62 +4,17 @@ using Portfolio.Client.Code;
 
 namespace Portfolio.Client.Shared
 {
-    public partial class Menu : IDisposable
+    public partial class Menu
     {
-        [Inject] public NavigationManager NavManager { get; set; } = default!;
+        [Parameter] public bool Open { get; set; }
+        [Parameter] public List<MenuItem> MenuItems { get; set; } = new();
 
         [Parameter] public Theme? Theme { get; set; }
         [Parameter] public EventCallback<Theme?> ThemeChanged { get; set; }
 
-        private List<MenuItem> MenuItems => MenuProvider.MenuItems;
-
-        private bool menuOpen;
-
-        private MenuItem? selected;
-
-        protected override async Task OnInitializedAsync()
-        {
-            NavManager.LocationChanged += NavManager_LocationChanged;
-            await SetMenuOpen();
-        }
-
-        private async void NavManager_LocationChanged(object? sender, LocationChangedEventArgs e)
-        {
-            // Update the selected nav item & rerender
-            await SetMenuOpen();
-            StateHasChanged();
-        }
-
         private async Task SetTheme(Theme? theme)
         {
-            // If the menu is closed, don't change the theme from any mouseover events
-            if (!menuOpen) return;
-
             await ThemeChanged.InvokeAsync(theme);
-        }
-
-        private async Task SetMenuOpen()
-        {
-            var uri = new Uri(NavManager.Uri);
-            var selectedItem = MenuItems.FirstOrDefault(i => uri.LocalPath.Contains(i.Url));
-
-            menuOpen = selectedItem == null;
-
-            // If the menu has been closed, update the selected menu item to play the shown animation
-            if (!menuOpen)
-            {
-                await ThemeChanged.InvokeAsync(selectedItem.Theme);
-                selected = selectedItem;
-            }
-            else
-            {
-                await ThemeChanged.InvokeAsync(null);
-            }
-        }
-
-        public void Dispose()
-        {
-            NavManager.LocationChanged -= NavManager_LocationChanged;
         }
     }
 }
