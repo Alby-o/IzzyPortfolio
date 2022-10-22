7b92f8666fa58c386bdfd6bb949851c40cf83712
Alexander Blyth <alex@3cssoftware.com.au>
2022-10-22 16:32:20 +1000

    Add work item list styles

--- Portfolio/Client/Util/DateOnlyExtensions.cs
+++ Portfolio/Client/Util/DateOnlyExtensions.cs
@@ -1,10 +1,15 @@
-﻿namespace Portfolio.Client.Code
+﻿namespace Portfolio.Client.Util
 {
     public static class DateOnlyExtensions
     {
         public static string ToMonthYearString(this DateOnly date)
         {
             return date.ToString("MMM yyyy");
+        }
+
+        public static string ToYearString(this DateOnly date)
+        {
+            return date.ToString("yyyy");
         }
     }
 }
